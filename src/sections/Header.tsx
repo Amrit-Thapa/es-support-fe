167f1548c747decc6cc49862e6c5fa788683b8c3
Amrit Thapa <amritthapa966@gmail.com>
2024-03-03 21:52:02 +0530

    updating

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,8 +1,18 @@
 import React from "react";
+import Logo from "@/../public/assets/es-support-log.png";
 
 const Header = () => {
   return (
-    <div className="h-14 md:h-24 w-full max-w-[1730px] shadow-lg">header</div>
+    <div className="h-14 md:h-24 shadow-lg">
+      <div className="flex items-center my-auto">
+        <img
+          src={Logo.src}
+          width={72}
+          height={54}
+          className="h-[54px] w-[72px] object-contain"
+        />
+      </div>
+    </div>
   );
 };
 
