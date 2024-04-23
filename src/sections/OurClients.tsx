06d0c31bdbb6207fbd637ffbd2955e9ac41b68f1
Amrit Thapa <amritthapa966@gmail.com>
2024-04-23 19:05:19 +0530

    adding partner logos

--- src/sections/OurClients.tsx
+++ src/sections/OurClients.tsx
@@ -2,7 +2,35 @@ import Csri from "@/../public/assets/images/csir.png";
 import genpact from "@/../public/assets/images/Genpact.png";
 import hcl from "@/../public/assets/images/HCL.png";
 import future from "@/../public/assets/images/Future-group.png";
-import "@/app/globals.css";
+import amrita from "@/../public/assets/images/amrita.png";
+import bhel from "@/../public/assets/images/BHEL.png";
+import cognizant from "@/../public/assets/images/cognizant.png";
+import dmrc from "@/../public/assets/images/dmrc.png";
+import drdo from "@/../public/assets/images/DRDO.png";
+import eli from "@/../public/assets/images/EIL.png";
+import fortis from "@/../public/assets/images/Fortis.png";
+import indianOil from "@/../public/assets/images/Indian-Oil.png";
+import maxLife from "@/../public/assets/images/Max-Life.png";
+import pnb from "@/../public/assets/images/pnb.png";
+import samsumg from "@/../public/assets/images/Samsung.png";
+
+const partnerList = [
+  Csri.src,
+  genpact.src,
+  hcl.src,
+  future.src,
+  amrita.src,
+  bhel.src,
+  cognizant.src,
+  dmrc.src,
+  drdo.src,
+  eli.src,
+  fortis.src,
+  indianOil.src,
+  maxLife.src,
+  pnb.src,
+  samsumg.src,
+];
 
 const OurClients = () => {
   return (
@@ -12,11 +40,10 @@ const OurClients = () => {
           Our Clients
         </div>
         <div className="mt-10">
-          <div className="whitespace-nowrap h-[42px] overflow-x-auto sm:scroll-box flex space-x-4">
-            <img src={Csri.src} className="" />
-            <img src={genpact.src} className="" />
-            <img src={hcl.src} className="" />
-            <img src={future.src} className="" />
+          <div className="whitespace-nowrap h-[42px] overflow-x-auto sm:scroll-box flex space-x-4 no-scrollbar">
+            {partnerList.map((partner) => (
+              <img src={partner} className="" />
+            ))}
           </div>
         </div>
       </div>
